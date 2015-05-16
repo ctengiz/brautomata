3c527b7e5799abec0d482a39a49ee6a5f8c3acf3
Çağatay Tengiz <cagatay.tengiz@makki.com.tr>
2015-05-16 17:48:56 +0300

    Performance improvements.
    Class operations are rather slow in Brython. See this thread : https://groups.google.com/forum/#!topic/brython/e7FIP7_2v28
    
    This update is based on Pierre Quentel's modifications.

--- automata.py
+++ automata.py
@@ -4,202 +4,218 @@ import datetime
 
 from browser import document, timer
 
+canvas = document["my-canvas"]
+ctx = canvas.getContext("2d")
+#self.ctx.translate(.5, .5)
+fillRect = ctx.fillRect
 
-class Gol:
-    def __init__(self):
-        self.canvas = document["my-canvas"]
-        self.ctx = self.canvas.getContext("2d")
-        #self.ctx.translate(.5, .5)
+is_active = False
+in_tick = False
 
-        self.is_active = False
-        self.in_tick = False
+grid = []
+initial_state = []
+tick_count = 0
+dead = 0
+alive = 0
 
-        self.grid = []
-        self.initial_state = []
-        self.tick_count = 0
-        self.dead = 0
-        self.alive = 0
+line_width = 1
+alive_cell_color = "#00FF00"
+dead_cell_color = "#e5e5e5"
+blank_cell_color = "#FFFFFF"
+grid_color = "#808080"
 
-        self.line_width = 1
-        self.alive_cell_color = "#00FF00"
-        self.dead_cell_color = "#e5e5e5"
-        self.blank_cell_color = "#FFFFFF"
-        self.grid_color = "#808080"
+document["btn_start_stop"].disabled = "disabled"
+document["btn_tick"].disabled = "disabled"
+document["btn_clear"].disabled = "disabled"
 
-        self.init_grid()
-        self.update_canvas()
+cols, rows, size, seed_ratio, rule, dead, alive, tick_delay = 0, 0, 0, 0, 0, 0, 0, 0
 
-        document["btn_start_stop"].disabled = "disabled"
-        document["btn_tick"].disabled = "disabled"
-        document["btn_clear"].disabled = "disabled"
+def init_grid(ev=None):
+    global cols, rows, size, seed_ratio, rule, canvas, tick_count, grid
+    print(">> init_grid", datetime.datetime.utcnow())
 
-    def init_grid(self, ev=None):
-        print(">> init_grid", datetime.datetime.utcnow())
+    cols = int(document["edt_cols"].value)
+    rows = int(document["edt_rows"].value)
+    size = int(document["edt_size"].value)
+    seed_ratio = int(document["edt_seed"].value)
 
-        self.cols = int(document["edt_cols"].value)
-        self.rows = int(document["edt_rows"].value)
-        self.size = int(document["edt_size"].value)
-        self.seed_ratio = int(document["edt_seed"].value)
+    #Rule is defined as S/B notation.
+    #Some well-known rules :
+    #23/3       : Conway's game of life
+    #1234/3     : Mazetric
+    #12345/3    : Maze
+    #For more info : http://www.conwaylife.com/wiki/Cellular_automaton
+    rule = document["edt_rule"].value
 
-        #Rule is defined as S/B notation.
-        #Some well-known rules :
-        #23/3       : Conway's game of life
-        #1234/3     : Mazetric
-        #12345/3    : Maze
-        #For more info : http://www.conwaylife.com/wiki/Cellular_automaton
-        self.rule = document["edt_rule"].value
+    tick_count = 0
 
-        self.tick_count = 0
+    canvas.width = cols * size
+    canvas.height = rows * size
 
-        self.canvas.width = self.cols * self.size
-        self.canvas.height = self.rows * self.size
+    document["btn_start_stop"].disabled = "disabled"
+    document["btn_tick"].disabled = "disabled"
+    document["btn_clear"].disabled = "disabled"
 
-        document["btn_start_stop"].disabled = "disabled"
-        document["btn_tick"].disabled = "disabled"
-        document["btn_clear"].disabled = "disabled"
+    grid = [[0 for x in range(cols)] for y in range(rows)]
+    print("<< init_grid", datetime.datetime.utcnow())
 
-        self.grid = [[0 for x in range(self.cols)] for y in range(self.rows)]
-        print("<< init_grid", datetime.datetime.utcnow())
+def update_canvas():
+    global dead, alive
+    dead = 0
+    alive = 0
 
-    def put_rect(self, rw, cl, color):
-        x1 = cl * self.size
-        y1 = rw * self.size
+    print(">> update_canvas", datetime.datetime.utcnow())
+    rw = 0
+    for rw in range(rows):
+        for cl in range(cols):
+            if grid[rw][cl]:
+                color = alive_cell_color
+                alive += 1
+            else:
+                color = dead_cell_color
+                dead += 1
 
-        self.ctx.fillStyle = color
-        self.ctx.fillRect(x1, y1, self.size, self.size)
+            x1 = cl * size
+            y1 = rw * size
+            ctx.fillStyle = color
+            fillRect(x1, y1, size, size)
 
-    def update_canvas(self):
-        self.dead = 0
-        self.alive = 0
+    update_labels()
+    print("<< update_canvas", datetime.datetime.utcnow())
 
-        print(">> update_canvas", datetime.datetime.utcnow())
-        rw = 0
-        while rw < self.rows:
-            cl = 0
-            while cl < self.cols:
-                if self.grid[rw][cl]:
-                    color = self.alive_cell_color
-                    self.alive += 1
-                else:
-                    color = self.dead_cell_color
-                    self.dead += 1
+def seed(ev):
+    global initial_state, alive, dead
+    
+    print(">> seed", datetime.datetime.utcnow())
 
-                self.put_rect(rw, cl, color)
+    init_grid()
+    
+    alive = 0
 
-                cl = cl + 1
-            rw = rw + 1
+    for rw in range(rows):
+        for cl in range(cols):
 
-        self.update_labels()
-        print("<< update_canvas", datetime.datetime.utcnow())
+            seed_chance = random.randint(1, 100)
 
-    def seed(self, ev):
-        print(">> seed", datetime.datetime.utcnow())
+            if seed_chance <= seed_ratio:
+                grid[rw][cl] = 1
+                alive += 1
+                color = alive_cell_color
+            else:
+                grid[rw][cl] = 0 #empty cell
+                color = dead_cell_color
+            x1 = cl * size
+            y1 = rw * size
+            ctx.fillStyle = color
+            fillRect(x1, y1, size, size)
+            
+    dead = (rows * cols) - alive
+    update_labels()
 
-        self.init_grid()
+    document["btn_start_stop"].disabled = ""
+    document["btn_tick"].disabled = ""
+    document["btn_clear"].disabled = ""
 
-        for rw in range(self.rows):
-            for cl in range(self.cols):
+    initial_state = deepcopy(grid)
+    print("<< seed", datetime.datetime.utcnow())
 
-                seed_chance = random.randint(1, 100)
-
-                if seed_chance <= self.seed_ratio:
-                    self.grid[rw][cl] = 1
-                else:
-                    self.grid[rw][cl] = 0 #empty cell
-
-        self.update_canvas()
+def start_stop(ev):
+    global is_active, tick_delay
+    if is_active:
+        is_active = False
+        document["btn_start_stop"].text = "Start"
 
         document["btn_start_stop"].disabled = ""
         document["btn_tick"].disabled = ""
         document["btn_clear"].disabled = ""
+    else:
+        tick_delay = int(document["edt_delay"].value)
+        is_active = True
+        document["btn_start_stop"].text = "Stop"
 
-        self.initial_state = deepcopy(self.grid)
-        print("<< seed", datetime.datetime.utcnow())
+        document["btn_start_stop"].disabled = ""
+        document["btn_tick"].disabled = "disabled"
+        document["btn_clear"].disabled = "disabled"
 
-    def start_stop(self, ev):
-        if self.is_active:
-            self.is_active = False
-            document["btn_start_stop"].text = "Start"
+        tick()
+        timer.set_timeout(tick, tick_delay)
 
-            document["btn_start_stop"].disabled = ""
-            document["btn_tick"].disabled = ""
-            document["btn_clear"].disabled = ""
-        else:
-            self.tick_delay = int(document["edt_delay"].value)
-            self.is_active = True
-            document["btn_start_stop"].text = "Stop"
+def update_labels():
+    document["lbl_tickno"].text = "Round: %d" %(tick_count)
+    document["lbl_alive"].text = "Alive: %d" %(alive)
+    document["lbl_dead"].text = "Dead: %d" %(dead)
 
-            document["btn_start_stop"].disabled = ""
-            document["btn_tick"].disabled = "disabled"
-            document["btn_clear"].disabled = "disabled"
+def tick(ev=None):
+    global in_tick, alive, dead, tick_count
 
-            self.tick()
-            timer.set_timeout(self.tick, self.tick_delay)
+    if in_tick:
+        return
 
-    def update_labels(self):
-        document["lbl_tickno"].text = "Round: %d" %(self.tick_count)
-        document["lbl_alive"].text = "Alive: %d" %(self.alive)
-        document["lbl_dead"].text = "Dead: %d" %(self.dead)
+    in_tick = True
 
-    def tick(self, ev=None):
-        if self.in_tick:
-            return
+    alive = 0
+    dead = 0
 
-        self.in_tick = True
+    #because slicing does not copy if a list is contained in a list !
+    old_grid = deepcopy(grid)
+    
+    _sr, _br = rule.split('/')
+    _sr = [int(x) for x in _sr]
+    _br = [int(x) for x in _br]
 
-        self.alive = 0
-        self.dead = 0
+    for rw in range(rows):
+        for cl in range(cols):
+            n_alive = 0
 
-        #because slicing does not copy if a list is contained in a list !
-        old_grid = deepcopy(self.grid)
+            for x in range(-1, 2):
+                nx = cl + x
+                if nx < 0:
+                    nx = cols - 1
+                elif nx >= cols:
+                    continue
 
-        _sr, _br = self.rule.split('/')
-        _sr = [int(x) for x in _sr]
-        _br = [int(x) for x in _br]
+                for y in range(-1, 2):
+                    ny = rw + y
 
-        rw = 0
-        while rw < self.rows:
-            cl = 0
-            while cl < self.cols:
-                n_alive = 0
+                    if ny < 0:
+                        ny = rows - 1
 
-                for x in range(-1, 2):
-                    for y in range(-1, 2):
-                        nx = cl + x
-                        ny = rw + y
+                    if (ny < rows) and ((x != 0) or (y != 0)):
+                        if old_grid[ny][nx] == 1:
+                            n_alive += 1
 
-                        if nx < 0:
-                            nx = self.cols - 1
-                        if ny < 0:
-                            ny = self.rows - 1
-
-                        if (nx >= 0) and (ny >= 0) and (nx < self.cols) and (ny < self.rows) and ((x != 0) or (y != 0)):
-                            if old_grid[ny][nx] == 1:
-                                n_alive += 1
-
-                if self.grid[rw][cl] == 1:
-                    #if cell is alive !
-                    if not(n_alive in _sr):
-                        self.grid[rw][cl] = 0
+            if grid[rw][cl] == 1:
+                #if cell is alive !
+                if not(n_alive in _sr):
+                    grid[rw][cl] = 0
+                    dead += 1
+                    x1 = cl * size
+                    y1 = rw * size
+                    ctx.fillStyle = dead_cell_color
+                    fillRect(x1, y1, size, size)
+            else:
+                if n_alive in _br:
+                    grid[rw][cl] = 1
+                    x1 = cl * size
+                    y1 = rw * size
+                    ctx.fillStyle = alive_cell_color
+                    fillRect(x1, y1, size, size)
                 else:
-                    if n_alive in _br:
-                        self.grid[rw][cl] = 1
+                    dead += 1
 
-                cl = cl + 1
-            rw = rw + 1
+    alive = (rows * cols) - dead
+    in_tick = False
+    tick_count += 1
 
-        self.in_tick = False
-        self.tick_count += 1
+    update_labels()
 
-        self.update_canvas()
+    if is_active:
+        timer.set_timeout(tick, tick_delay)
 
-        if self.is_active:
-            timer.set_timeout(self.tick, self.tick_delay)
+init_grid()
+update_canvas()
 
-
-gol = Gol()
-
-document['btn_seed'].bind('click', gol.seed)
-document['btn_start_stop'].bind('click', gol.start_stop)
-document['btn_tick'].bind('click', gol.tick)
-document['btn_clear'].bind('click', gol.init_grid)
+document['btn_seed'].bind('click', seed)
+document['btn_start_stop'].bind('click', start_stop)
+document['btn_tick'].bind('click', tick)
+document['btn_clear'].bind('click', init_grid)
